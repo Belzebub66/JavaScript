7a4ff3df1dc2fc7de82081e3e9c69a4509249413
Leviathan <l23920238@voaxaca.tecnm.mx>
2025-04-10 20:41:56 -0600

    cambios de la clase 10-04-25

--- 3.4.1_D/script.js
+++ 3.4.1_D/script.js
@@ -12,7 +12,23 @@ function addTaskFunc() {
     let taskList = document.getElementById("taskList");
     //creating a new li element
     let newLi = document.createElement("li");
-    newLi.innerText = addTaskInput.value;
+    //newLi.innerText = addTaskInput.value;
+
+    //creating a paragraph element
+    let newP = document.createElement("p"); 
+    newP.innerText = addTaskInput.value;
+    newLi.appendChild(newP);
+
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Delete";
+    
+    buttonDelete.addEventListener("click", () => {
+        //taskList.removeChild(newLi);
+        newLi.remove(); //removes the li element from the ul element
+    });
+
+    newP.appendChild(buttonDelete);
+
 
     taskList.appendChild(newLi); //appending the new li element to the ul element
     
@@ -37,3 +53,72 @@ this method receives 2 arguments
 */
 
 addTask.addEventListener("click", addTaskFunc);
+
+// a function is a code block of reusable code that performs a specific task
+function suma(a, b) {
+    return a + b;
+}
+let result = suma(2, 3);
+
+//anonymous function its known as an arrow function
+/*
+(param1, param2, paramn) =>{
+    //code block
+    return something;
+}
+
+doesnt have funcion and namenctions is to use them 
+
+the idea of these fu
+*/ 
+//example
+let sumfunction = (a, b) => {
+    return a + b;
+}
+
+sumfunction(5,1);
+
+
+
+/*
+IN WEB DEVELOPMENT WE USE THE CONCEPT OF COMPONENTS
+Is a building block for graphical user interface (GUI) applications.
+*/
+let i=0;
+let subtitle = "This is a subtitle" + i;
+let paragraph = "This is a paragraph" + i;
+
+
+function subtitleandP(){
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = 
+    "<h2>" + subtitle + "</h2>"
+    "<p>" + paragraph + "</p>"
+    ;
+    return newDiv;
+}
+for (i = 0; i < 3; i++) {
+    document.body.appendChild(subtitleandP());
+}
+
+`text ${i}`
+//the pages are not created manually
+//they are created by a function that creates the page and returns it
+
+console.log(document.getElementsByTagName("h2"));
+
+let elementsh2blue = document.getElementsByTagName("h2blue");
+for(let j = 0; j < elementsh2blue.lenght; j++){
+    elementsh2blue[j].style.backgroundcolor = "green";
+}
+
+/* 
+how to name things in programming
+1. camelCase -> for variables and functions
+2. PascalCase -> for classes
+3. snake_case -> for files and folders
+4. kebab-case
+
+for our final project we must use camelCase
+
+*/
